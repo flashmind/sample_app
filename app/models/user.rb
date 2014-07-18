bb0f15192e768735612e9648982f42d852f849a6
Flashmin <larryn@flashmind.net>
2014-07-18 14:22:14 -0400

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive:false },
                     uniqueness: true
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 									
 end
